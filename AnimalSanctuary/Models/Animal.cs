985da35e73e5ec68c7ee57207f9f3adc264437da
student <student@epicodus.com>
2018-04-26 09:34:03 -0700

    initialize project

---
+++ AnimalSanctuary/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalSanctuary.Models
+{
+    public class Animal
+    {
+        private int AnimalId;
+        private string Name;
+        private string Species;
+        private string Sex;
+        private string HabitatType;
+        private bool MedicalEmergency;
+        private int VetId;
+        private Vet Vet;
+    }
+}
---
+++ AnimalSanctuary/Models/Vet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalSanctuary.Models
+{
+    public class Vet
+    {
+        private int VetId;
+        private string Name;
+        private string Specialty;
+    }
+}
